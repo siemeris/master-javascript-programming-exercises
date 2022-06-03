e6307628aa571fc545358a6baafe0586ed4da51b
siemeris <siemeris@gmail.com>
2022-06-03 13:27:39 +0000

    Exercise 159

--- exercises/159.4-FashionInventory-D/app.js
+++ exercises/159.4-FashionInventory-D/app.js
@@ -1,30 +1,38 @@
 function getLaceNameDataForShoes(inventory) {
 
   var nameWords;
+  var nameWordss;
   var targetWordIndex;
   var obj={};
   var newArray = [];
+  var j=0;
   if (typeof inventory === "undefined"){
     return []
   }
   else{
   for (var i = 0; i < inventory.length; i++) {
+    console.log(inventory[i].name);
     for (var n = 0; n < inventory[i].shoes.length; n++) {
       if (inventory[i].shoes[n].name.includes('lace')){
         nameWords = inventory[i].shoes[n].name.split(' ');
         console.log(nameWords);
+        //obj[i] = nameWords;
         for (let h = 0; h < nameWords.length; h++) {
           if (nameWords[h].includes('lace') == true) {
             targetWordIndex = h;
+            console.log(targetWordIndex);
+            obj['targetWordIndex'] = targetWordIndex;
           }
         }
+        newArray.push({nameWords: nameWords, targetWordIndex: targetWordIndex});
       } 
-      obj.nameWords = nameWords;
-      obj.targetWordIndex = targetWordIndex;
-      newArray.push(obj);
-    }
-    return newArray
+      //obj.nameWords = nameWords;
+      //obj.targetWordIndex = targetWordIndex;
+      
+    } 
+     
   }
+return newArray
 }  
 }
 
