7e4c0111897bbd937ed9910bd11ea47e1237e4f4
siemeris <siemeris@gmail.com>
2022-06-04 09:49:41 +0200

    Update app.js

--- exercises/154-searchWithDivideAndConquer/app.js
+++ exercises/154-searchWithDivideAndConquer/app.js
@@ -1,26 +1,27 @@
 function search(array, value) {
   var indIni = 0;
   var indFin = array.length; 
-  var m = indIni + (indFin - indIni) / 2;
-  // your code here
-  while (indIni <= indFin) {
-    m = indIni + (indFin - indIni) / 2;
+  var indMedio = Math.floor(((indFin - indIni)) / 2);
 
-      if (array[m] == value){
-        return array[m]
-      }
-      else if (array[m] <value){
-        
-      }
-      else if (array[m] >value){
-        
-      }
-      else if (value < array[Math.floor(array.length/2)]){
-        newArray = array.slice(0, value);
-        search(newArray,value);
-      }
+  // your code here
+  if (value > array[indFin-1] || value < array [indIni]) {
+    return null
   }
+  while (indIni <= indFin) {
+      if (array[indMedio] == value){
+        return indMedio
+      }
+      else if (array[indMedio]<value){
+        indFin = array.length-1;
+        indIni = indMedio;
+        indMedio = indIni + Math.floor(((indFin - indIni)) / 2);
+      }
+      else if (array[indMedio]>value){
+        indFin = indMedio;
+        indMedio = indIni + Math.floor(((indFin - indIni)) / 2);
+      }
+      }
 }
 
 var arr = [1, 3, 16, 22, 31, 33, 34]
-console.log(search(arr, 31)); // => 4
+console.log(search(arr, 34)); // => 4
